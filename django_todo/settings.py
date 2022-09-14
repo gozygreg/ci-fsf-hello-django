e8e42b7a6ed4d95711e21c7f0839683204c3ecd2
gozygreg <goziennabuife@yahoo.com>
2022-09-14 18:39:22 +0000

    Create secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'itjf(h5r2+))wzasun0wf-#v@wzwz&w*%^2aggugn_=x(u#6mz')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
@@ -85,7 +85,7 @@ if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
